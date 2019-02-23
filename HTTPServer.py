1160f70afc4b8e003a0bafad44a1987c0a12145a
Javier Albo <javier@Jay-Mac.local>
2019-02-22 21:41:22 -0800

    Added config param

--- HTTPServer.py
+++ HTTPServer.py
@@ -9,15 +9,24 @@ config = configparser.ConfigParser()
 config.read('conf/httpd.conf')
 
 # pyHTTPServer Class
-class PyHTTPServer:
+class PyHTTPServer(object):
     def __init__(self):
         # Init socket
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        self.sock.bind((config['pyHTTP']['host'], int(config['pyHTTP']['port'])))    
+        try:
+            # Try to create socket and bind host & port
+            self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            self.sock.bind((config['pyHTTP']['host'], int(config['pyHTTP']['port'])))    
+
+        except socket.error:
+            # Unable to create socket
+            print("Error: Can't create socket, port already in use")
+            sys.exit()
+    
+    def srvListen(self):
+        # Socket listen
+        self.sock.listen(int(config['pyHTTP']['maxOutstandingCon']))
         print("pyHTTP Server running on %s:%s" % (config['pyHTTP']['host'], config['pyHTTP']['port']))
 
-
 # Main
-
-x = PyHTTPServer()
+PyHTTPServer().srvListen()
